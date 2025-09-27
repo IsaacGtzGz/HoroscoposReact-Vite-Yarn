9e65308e9f5270e7b51419899b7abeb2258dbdc1
IsaacGtzGz <isaacgtzgz@gmail.com>
2025-09-27 10:18:29 -0600

    fix: Eliminar referencia a iconos PWA no existentes en vite.config.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cuh19cvf098"
+    "revision": "0.bl3d84kh0dg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
