bacbb197e4abcc8236a74c6338f13723686c19ce
IsaacGtzGz <isaacgtzgz@gmail.com>
2025-09-30 11:40:59 -0600

    feat: Integración final

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import ReloadPrompt from './ReloadPrompt.jsx' // <-- Añade esta línea
+import ReloadPrompt from './ReloadPrompt.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
